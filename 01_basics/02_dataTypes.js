0c91a816ccde00a29805f7c1d1e2ee5bcf4fe7f3
focusedaf <126765236+focusedaf@users.noreply.github.com>
2024-05-01 15:56:44 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treats all js wrt to newer version
+
+//alert(3+3)// we are using nodejs not browser 
+
+let name ="luard"
+let age = 18
+let isLoggedIn = false
+
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+//object
+
+console.log(typeof null); //oject
+console.log(typeof undefined);//undefined
