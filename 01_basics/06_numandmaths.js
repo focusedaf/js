2f977f4d808b8f3f4d2a9921179c52ca1d9940e4
focusedaf <126765236+focusedaf@users.noreply.github.com>
2024-05-03 05:09:05 +0000

    numandmaths

---
+++ 01_basics/06_numandmaths.js
@@ -0,0 +1,17 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(200)
+//console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 3.2344
+//console.log(otherNumber.toPrecision(2));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//maths
+console.log(Math); 
