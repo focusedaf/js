f80e35eac42881499985135afc0c5e2aa59f7255
focusedaf <126765236+focusedaf@users.noreply.github.com>
2024-05-03 03:10:44 +0000

    comaprisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,17 @@
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+
+// === it checks the datatype along with the value
+
+console.log("2" === 2);
+console.log(null === undefined);
+console.log("luard" === "Luard");
+console.log("luard" === "LUARD");
