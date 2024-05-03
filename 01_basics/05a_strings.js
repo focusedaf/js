ac50e62e17b39db5dec400b05891a0e08176350f
focusedaf <126765236+focusedaf@users.noreply.github.com>
2024-05-03 04:39:03 +0000

    strings

---
+++ 01_basics/05a_strings.js
@@ -0,0 +1,22 @@
+const name= "luard"
+const repoCount =  7
+//console.log(name + repoCount + " value");
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new  String('minecraft')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length); //length is a function
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf('r'));
+
+//trimming the spaces
+const stringOne =" luard "
+console.log(stringOne);
+console.log(stringOne.trim());
+
+//replace to change something with something else
+
+//split divides the string based on the separators
