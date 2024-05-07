b345f8ee5500620ecc1848220cd977bdeea68860
focusedaf <126765236+focusedaf@users.noreply.github.com>
2024-05-07 20:30:21 +0000

    loops

---
+++ 02_controlflow/loops.js
@@ -0,0 +1,15 @@
+// for
+
+// for (let index = 0; index < 10; index++) {
+//     const element = i;
+//    // console.log(element);
+// }
+
+for (let i = 0; i < 10; i++) {
+    console.log('outer loop');
+    for (let j = 0; j < 10; j++) {
+      
+        console.log('inner loop');
+    }
+    
+}
